b8ce0a0e254d52f7d4aeb830630f6e3b8c1bd714
GMitchell98 <g.mitchell9@rgu.ac.uk>
2020-10-22 18:35:05 +0100

    updated dependancies and fixed a spelling error

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1,101);
     loop{
         println!("Please input your guess.");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failled to read line");
+            .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse(){
                     Ok(num) => num,
